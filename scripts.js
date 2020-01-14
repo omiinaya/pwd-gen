055c456287b3931e115c398afeaeaf06dd27b044
Omar Minaya <omiinaya@gmail.com>
2020-01-14 12:54:07 -0800

    general code cleanup.

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ let numeric = ["0","1","2","3","4","5","6","7","8","9"];
 let lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let charset = [];
+let length = 0;
 
 function specialFn() {
     var checkBox = document.getElementById("specialCheck");
@@ -61,9 +62,15 @@ function upperFn() {
     }
 }
 function setLength() {
-    var pwdLength=document.getElementById("length");
-    alert(pwdLength.value);
+    var pwdLength=document.getElementById("lengthCheck");
+    length = pwdLength.value;
+    console.log(length);
 }
 function generatePassword() {
-            //filler
+    for (var i=0, n=charset.length; i < length; ++i) {
+        var content = charset[Math.floor(Math.random()*n)];
+}
+    document.getElementById("password").innerHTML = content;
+        console.log(charset);
+        console.log(length);
 }
