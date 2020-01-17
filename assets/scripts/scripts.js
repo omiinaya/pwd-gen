09c30199106b984cc6fd2f4b3748bedc3cdac047
Omar Minaya <omiinaya@gmail.com>
2020-01-17 04:42:43 -0800

    added a fail check to copyFn incase there is no text to copy.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,7 +3,7 @@ let numeric = ["0","1","2","3","4","5","6","7","8","9"];
 let lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let charset = [];
-let newLength = 8;
+let newLength = 0;
 
 function arrayMerge(a, b) {
     if (document.getElementById(a).checked == true){
@@ -15,10 +15,15 @@ function arrayMerge(a, b) {
     }
 }
 function copyFn() {
-    var copyText = document.getElementById("password");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
+    if ((document.getElementById("password").value) == "") {
+        alert("There is nothing to copy yet.");
+    } else {
+        var copyText = document.getElementById("password");
+        copyText.select();
+        copyText.setSelectionRange(0, 99999);
+        document.execCommand("copy");
+        console.log(document.getElementById("password").value);
+    }
 }
 function setLength() {
     var pwdLength=document.getElementById("lengthCheck");
